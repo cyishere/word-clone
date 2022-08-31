14d43120c211d7e1385cf45939c27267e254cf0c
Chen Yang <cyishere@gmail.com>
2022-08-31 14:17:11 +0800

    Exercise 4: add game logic

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -3,13 +3,17 @@ import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import { range } from "../../utils";
 import Guess from "../Guess";
 
-function GuessResults({ guesses }) {
+function GuessResults({ guesses, answer }) {
   const rows = range(0, NUM_OF_GUESSES_ALLOWED);
 
   return (
     <div className="guess-results">
       {rows.map((i) => (
-        <Guess key={i} guess={guesses.length > 0 ? guesses[i] : ""} />
+        <Guess
+          key={i}
+          answer={answer}
+          guess={guesses.length > 0 ? guesses[i] : ""}
+        />
       ))}
     </div>
   );
