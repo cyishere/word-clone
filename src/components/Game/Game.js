5b2ce799ad24c1ed84f651ae35e22ee7490a04e0
Chen Yang <cyishere@gmail.com>
2022-08-31 14:00:39 +0800

    Add guess slots

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,7 @@ import { sample } from "../../utils";
 import { WORDS } from "../../data";
 import WordInput from "../WordInput";
 import GuessResults from "../GuessResults";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -14,6 +15,8 @@ function Game() {
   const [guesses, setGuesses] = useState([]);
 
   const addGuess = (word) => {
+    if (guesses.length >= NUM_OF_GUESSES_ALLOWED) return;
+
     setGuesses([...guesses, word]);
     console.log({ word });
   };
